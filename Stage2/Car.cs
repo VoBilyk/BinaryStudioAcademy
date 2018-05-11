c9ecb4f1d38519328cdd3f8cbc40febefeb2931b
Volodymyr <34250821+VoBilyk@users.noreply.github.com>
2018-05-11 10:16:05 +0300

    add logic to class Car

--- Stage2/Car.cs
+++ Stage2/Car.cs
@@ -1,12 +1,37 @@
 ﻿
+using System;
+
 namespace Stage2
 {
     class Car
     {
-        public int Id { get; set; }
+        public Car(int id, CarType type, decimal balance = 0)
+        {
+            Id = id;
+            Type = type;
+            Balance = balance;
+        }
 
-        public decimal Balance { get; set; }
+        public int Id { get; private set; }
 
-        public CarType Type { get; set; }
+        public decimal Balance { get; private set; }
+
+        public CarType Type { get; private set; }
+
+        public void AddMoney(decimal value)
+        {
+            if (value < 0)
+                throw new InvalidOperationException("Wrong value");
+            else
+                Balance += value;
+        }
+
+        public void SubMoney(decimal value)
+        {
+            if (Balance < value && value < 0)
+                throw new InvalidOperationException("Wrong value");
+            else
+                Balance -= value;
+        }
     }
 }
